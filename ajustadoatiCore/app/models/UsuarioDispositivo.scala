7ae337f2d6cca8893b8067fd5548d92269181715
ajustadoati <richardroj@icloud.com>
2018-02-17 15:09:08 +0100

    delete usuariodispositivo

--- ajustadoatiCore/app/models/UsuarioDispositivo.scala
+++
@@ -1,9 +0,0 @@
-package models.usuarioDispositivo
-
-import play.api.libs.json.Json
-import models.usuario.Usuario
-import models.dispositivo.Dispositivo
-
-
- case class UsuarioDispositivo(usuario:String, dispositivo:Dispositivo)
-
