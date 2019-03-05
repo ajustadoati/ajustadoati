88f3ef1dccdaf3d3ee40c2b5e0ef1b6de9a346d1
Richard Rojas <richardroj@gmail.com>
2019-03-05 20:14:51 +0100

    Fixed user register

--- ajustadoatiCore/app/repositories/UsuarioRepository.scala
+++ ajustadoatiCore/app/repositories/UsuarioRepository.scala
@@ -10,7 +10,9 @@ import org.anormcypher._
 import play.api.Logger
 import models.connection.Connection
 
-
+/**
+** Interface User
+**/
 trait UsuarioRepositoryComponent {
     val usuarioRepository: UsuarioRepository
     
@@ -28,7 +30,7 @@ trait UsuarioRepositoryComponent {
         
     }
 }
-
+/**Class User **/
 trait UsuarioRepositoryComponentImpl extends UsuarioRepositoryComponent with Connection{
     override val usuarioRepository = new UsuarioRepositoryImpl
      
@@ -37,27 +39,25 @@ trait UsuarioRepositoryComponentImpl extends UsuarioRepositoryComponent with Con
     class UsuarioRepositoryImpl extends UsuarioRepository {
         
         
-        
+        /*
+        ** Method to create user
+        **/
        override def createUsuario(usuario: Usuario): Usuario = {
         
-
-         Logger.info("Guardando usuario"+usuario)
+            Logger.info("Repository: Saving user"+usuario)
             val fecha = System.currentTimeMillis()
-            //Cypher("""CREATE (cl:Cliente { nombre: {nombre}, email: {email}, twitter: {twitter}, telefono:{telefono}, latitud:{latitud}, longitud:{longitud}}) CREATE (pr:Producto {nombre:{pnombre}, descripcion:{descripcion}}) CREATE (cl)-[:BUSCO]->(pr)""").on("nombre"->cliente.nombre, "email"->cliente.email, "twitter"->cliente.twitter, "latitud"->cliente.latitud.toFloat, "longitud"->cliente.longitud.toFloat, "pnombre"->producto.nombre, "descripcion"->producto.descripcion).execute()
-            //CREATE (pr:Producto {nombre:{pnombre}, descripcion:{descripcion}})//, "pnombre"->producto.nombre, "descripcion"->producto.descripcion
-            val result = Cypher("""CREATE (aa:Usuario {nombre:{nombre}, email:{email}, latitud:{latitud}, longitud:{longitud}, user:{user}, password:{password}, telefono:{telefono}}, fecha:{fecha})""").on("nombre"->usuario.nombre, "email"->usuario.email,"latitud"->usuario.latitud.toFloat, "longitud"->usuario.longitud.toFloat, "user"->usuario.user, "password"->usuario.password, "telefono"->usuario.telefono, "fecha"->fecha).execute()
             
+            val result = Cypher("""CREATE (aa:Usuario {nombre:{nombre}, email:{email}, latitud:{latitud}, longitud:{longitud}, user:{user}, password:{password}, telefono:{telefono}, fecha:{fecha}})""").on("nombre"->usuario.nombre, "email"->usuario.email,"latitud"->usuario.latitud.toFloat, "longitud"->usuario.longitud.toFloat, "user"->usuario.user, "password"->usuario.password, "telefono"->usuario.telefono, "fecha"->fecha).execute()
              
             if(result==true)
                 return usuario
             else
                 return null
         
-            
         }
 
         override def tryFindByUser(user: String): Usuario = {
-            //Option(proveedores.get(id))
+            
             Logger.info("Buscando Usuario")         
             
             val allUsuarios= Cypher("MATCH (n:Usuario) WHERE n.user={user} RETURN n.nombre as nombre, n.email as email, n.latitud as latitud, n.longitud as longitud, n.user as user, n.password as password, n.telefono as telefono").on("user"->user)().map{     
@@ -69,8 +69,6 @@ trait UsuarioRepositoryComponentImpl extends UsuarioRepositoryComponent with Con
                     return lista.iterator.next
                 else
                     return null
-            
-        
         }
 
          override def tryFindByUserAndPassword(user: String, password:String): Usuario = {
@@ -86,8 +84,6 @@ trait UsuarioRepositoryComponentImpl extends UsuarioRepositoryComponent with Con
                     return lista.iterator.next
                 else
                     return null
-            
-        
         }
 
         override def addContactToUser(contact: String, user:String): String = {
@@ -100,8 +96,6 @@ trait UsuarioRepositoryComponentImpl extends UsuarioRepositoryComponent with Con
                 return "OK"
             else
                 return null
-            
-        
         }
 
         override def list(): List[Usuario]={
@@ -116,9 +110,6 @@ trait UsuarioRepositoryComponentImpl extends UsuarioRepositoryComponent with Con
                     return lista
                 else
                     return null
-            
-            
-           
         }
         
     }
